4db5dacfefdb958b5695c378558bf1c8ca628705
Nachiket Pusalkar <nachiket87@gmail.com>
2020-08-12 14:45:37 -0400

    for review forms to work

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+  
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+     
+  end
+
+  private 
+
+  def review_params
+    params.require(:restaurant).permit(:rating, :content)
+  end
+
 end
